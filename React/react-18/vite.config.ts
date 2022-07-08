ce176a35536d7db543fe5655d7a752945b3b6e1b
keliyoo <44566975+keliyoo@users.noreply.github.com>
2022-07-08 13:57:35 +0800

    1

--- React/react-18/vite.config.ts
+++ React/react-18/vite.config.ts
@@ -18,6 +18,6 @@ export default ({ mode }) => {
         server: {
             port: 10018,
         },
-        plugins: [react({jsxRuntime: 'automatic',})],
+        plugins: [react({jsxRuntime: 'classic',})],
     });
 };
